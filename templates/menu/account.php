9d951a1d57d2f1e54333c097c7ecf605e4d9ad54
Puleeno Nguyen <puleeno@gmail.com>
2020-01-02 00:03:29 +0700

    Show account avarar in menu item

--- templates/menu/account.php
+++ templates/menu/account.php
@@ -1,14 +1,21 @@
 <div class="dropdown">
 	<button class="btn btn-secondary dropdown-toggle" type="button" id="ramphor-user-account" data-toggle="dropdown"
 		aria-haspopup="true" aria-expanded="false">
-		Tài khoản
+		<img
+			src="<?php echo get_avatar_url( $currentUser->ID, array( 'size' => 30 ) ); ?>"
+			alt="<?php echo $currentUser->display_name; ?>"
+		/>
 	</button>
 	<div class="dropdown-menu" aria-labelledby="ramphor-user-account">
-		<h6 class="dropdown-header"><?php echo $currentUser->display_name; ?></h6>
-
+		<h5 class="dropdown-header"><?php echo $currentUser->display_name; ?></h6>
 		<a class="dropdown-item">Thông tin</a>
 		<a class="dropdown-item">Cài đặt</a>
 		<div class="dropdown-divider"></div>
-		<a class="dropdown-item" href="<?php echo ramphor_user_profile_url( 'logout' ); ?>?redirect=<?php echo $redirect; ?>">Đăng xuất</a>
+		<a
+			class="dropdown-item"
+			href="<?php echo ramphor_user_profile_url( 'logout' ); ?>?redirect=<?php echo $redirect; ?>"
+		>
+			Đăng xuất
+		</a>
 	</div>
 </div>
