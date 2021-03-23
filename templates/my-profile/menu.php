c63eafe4f00b681f2c5bc7c60d11117071161141
Puleeno Nguyen <puleeno@gmail.com>
2021-03-24 03:31:52 +0700

    Create default templates

---
+++ templates/my-profile/menu.php
@@ -0,0 +1,17 @@
+<div class="<?php echo $unique_id ?>-profile-menu">
+    <ul class="profile-menu">
+        <?php foreach($menu_items as $menu_item): ?>
+            <li class="profile-menu-item<?php if ($current_feature === $menu_item['type']) {
+                echo ' active';
+            } ?>">
+                <?php if(isset($menu_item['url'])): ?>
+                    <a href="<?php echo $menu_item['url']; ?>">
+                        <?php echo $menu_item['label']; ?>
+                    </a>
+                <?php else: ?>
+                    <?php echo $menu_item['label']; ?>
+                <?php endif; ?>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</div>
