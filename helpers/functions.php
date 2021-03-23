d1c8938c5d2889349d35b1ef421c5db3dceb03d2
Puleeno Nguyen <puleeno@gmail.com>
2021-03-24 03:27:49 +0700

    Create new functions `is_my_profile` and `ramphor_user_profile_template`

--- helpers/functions.php
+++ helpers/functions.php
@@ -10,3 +10,15 @@
 
 function ramphor_user_profile_url() {
 }
+
+if ( ! function_exists( 'is_my_profile' ) ) {
+    function is_my_profile() {
+        global $wp_query;
+        return isset($wp_query->is_ramphor_my_profile) && $wp_query->is_ramphor_my_profile;
+    }
+}
+
+
+function ramphor_user_profile_template($template, $data = array(), $id = null, $echo = true) {
+    return \Ramphor\User\UserTemplateLoader::render($template, $data, $id, $echo);
+}
