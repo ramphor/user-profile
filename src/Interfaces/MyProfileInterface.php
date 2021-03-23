2f17181ef4cfda947733fbde2cff8a1874f80d06
Puleeno Nguyen <puleeno@gmail.com>
2021-03-24 03:30:09 +0700

    Define `MyProfileInterface` and `MyProfileAbstract`

---
+++ src/Interfaces/MyProfileInterface.php
@@ -0,0 +1,11 @@
+<?php
+namespace Ramphor\User\Interfaces;
+
+interface MyProfileInterface
+{
+    public function getName();
+
+    public function getMenuItem();
+
+    public function render();
+}
