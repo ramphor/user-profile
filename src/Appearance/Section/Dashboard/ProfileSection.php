9fdfda03d9295a55d970e5fbc2e2685199fab587
Puleeno Nguyen <puleeno@gmail.com>
2021-03-25 23:47:38 +0700

    Support save user info

--- src/Appearance/Section/Dashboard/ProfileSection.php
+++ src/Appearance/Section/Dashboard/ProfileSection.php
@@ -23,5 +23,35 @@ class ProfileSection extends SectionAbstract
 
     public function save()
     {
+        if (!is_user_logged_in()) {
+            return false;
+        }
+
+        $user_data = array(
+            'ID' => get_current_user_id(),
+        );
+        $displayName = null;
+
+        if (!empty(($firstName = array_get($_POST, 'first_name')))) {
+            $user_data['first_name'] = $firstName;
+        }
+        if (!empty(($lastName = array_get($_POST, 'last_name')))) {
+            $user_data['last_name'] = $lastName;
+        }
+        if (!empty(($displayName = array_get($_POST, 'display_name')))) {
+            $user_data['display_name'] = $displayName;
+        }
+        if (!is_null($description = array_get($_POST, 'description'))) {
+            $user_data['description'] = $description;
+        }
+
+        $update_status = wp_update_user($user_data);
+        if (!is_wp_error($update_status) && $update_status > 0) {
+            global $current_user;
+            if ($displayName !== null) {
+                $current_user->display_name = $displayName;
+            }
+        }
+        return $update_status;
     }
 }
