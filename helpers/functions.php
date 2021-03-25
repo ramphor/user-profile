4a2a9c6558a56d8b9345f14228fa0a5ee32fd0f8
Puleeno Nguyen <puleeno@gmail.com>
2021-03-25 07:57:34 +0700

    Create a new function `ramphor_user_profile_my_profile_schemes`

--- helpers/functions.php
+++ helpers/functions.php
@@ -22,3 +22,27 @@ if ( ! function_exists( 'is_my_profile' ) ) {
 function ramphor_user_profile_template($template, $data = array(), $id = null, $echo = true) {
     return \Ramphor\User\UserTemplateLoader::render($template, $data, $id, $echo);
 }
+
+function ramphor_user_profile_my_profile_schemes() {
+    $schemes = array(
+        'aquatic' => 'Aquatic',
+        'blue' => 'Blue',
+        'classic-blue' => 'Classic Blue',
+        'classic-bright' => 'Classic Bright',
+        'classic-dark' => 'Classic Dark',
+        'coffee' => 'Coffee',
+        'contrast' => 'Contrast',
+        'ectoplasm' => 'Ectoplasm',
+        'light' => 'Light',
+        'midnight' => 'Midnight',
+        'modern' => 'Modern',
+        'nightfall' => 'Nightfall',
+        'ocean' => 'Ocean',
+        'powder-snow' => 'Powder Snow',
+        'sakura' => 'Sakura',
+        'sunrise' => 'Sunrise',
+        'sunset' => 'Sunset',
+    );
+
+    return apply_filters('ramphor_user_profile_schemes', $schemes);
+}
